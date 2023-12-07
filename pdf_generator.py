a257ff32391f46c40cffe78f40897ebbc1fbf188
gabrielkumar <64897211+gabrielkumar@users.noreply.github.com>
2023-12-07 10:43:18 +0100

    edit pdf

--- pdf_generator.py
+++ pdf_generator.py
@@ -1,70 +1,92 @@
 import os
 from flask import Flask, request, send_file
 from reportlab.lib.pagesizes import letter
-from reportlab.pdfgen import canvas
-from reportlab.lib import colors
-from base64 import b64decode
-from PIL import Image
-from io import BytesIO
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Table, Spacer, Image as PlatypusImage
+from reportlab.lib.styles import getSampleStyleSheet
+from PIL import Image as PILImage
 import datetime
+from io import BytesIO
+from base64 import b64decode
+from reportlab.lib.units import inch
 
 app = Flask(__name__)
 
 def generate_pdf(signature_data, file_path):
-    signature_image_data = b64decode(signature_data.split(',')[1])
+    signature_image = PILImage.open(BytesIO(b64decode(signature_data.split(',')[1])))
 
-    pdf = canvas.Canvas(file_path, pagesize=letter)
-    pdf.setFont("Helvetica-Bold", 16)
-    pdf.drawString(100, 750, "Mitgliedsantrag TSV Roßtal")
+    doc = SimpleDocTemplate(file_path, pagesize=letter, leftMargin=30, rightMargin=30)
+    story = []
 
-    pdf.setFont("Helvetica", 12)
-    table_data = [
-        ["Name:", ""],
+    styles = getSampleStyleSheet()
+    style_normal = styles["Normal"]
+    style_heading = styles["Heading2"]
+
+    # Membership type section...
+    membership_type_text = "Mitgliedschaft   Neumitgliedschaft   Umstellung Familienmitgliedschaft   Antrag auf Beitragsbefreiung"
+    story.append(Paragraph(f"O {membership_type_text}", style_normal))
+
+    # Tabelle für persönliche Informationen...
+    personal_info = [
+        ["Geschlecht:", ""],
         ["Vorname:", ""],
-        ["Adresse:", ""],
-        ["PLZ:", ""],
-        ["Ort:", ""],
-        ["Datum:", ""],
-        ["Unterschrift:", ""]
+        ["Name:", ""],
+        ["Geburtsdatum:", ""],
+        ["E-Mail:", ""],
+        ["Telefon/Mobil:", ""],
+        ["Ich bin bereit ehrenamtliche Tätigkeiten zu übernehmen", ""]
     ]
 
-    current_timestamp = datetime.datetime.now().strftime("%d.%m.%Y")
-    table_data[5][1] = current_timestamp
+    data = [
+        [Paragraph(item[0], style_normal), Paragraph(item[1], style_normal)] for item in personal_info
+    ]
 
-    for row_index, row in enumerate(table_data):
-        pdf.drawString(100, 700 - 50 * row_index, row[0])
-        if row_index == 5:
-            pdf.drawString(300, 700 - 50 * row_index, row[1])
-            pdf.setStrokeColor(colors.lightgrey)  # Hellgraue Rahmenfarbe
-            pdf.rect(300, 685 - 50 * row_index, 200, 50, stroke=1, fill=0)  # Anpassung der Höhe für die Unterschrift auf 50
-        else:
-            pdf.drawString(300, 700 - 50 * row_index, row[1])
-            pdf.setStrokeColor(colors.lightgrey)  # Hellgraue Rahmenfarbe
-            pdf.rect(300, 685 - 50 * row_index, 200, 50, stroke=1, fill=0)  # Beibehaltung der Höhe für andere Zellen auf 50
+    personal_table = Table(data, colWidths=[2*inch, 2*inch], rowHeights=0.25*inch)
+    personal_table.setStyle([('VALIGN', (0, 0), (-1, -1), 'MIDDLE')])
 
-        if row_index == 6:
-            signature_image = Image.open(BytesIO(signature_image_data))
-            image_width, image_height = signature_image.size
-            scale_factor = 190 / image_width if image_width > 0 else 1
-            scaled_width = image_width * scale_factor
-            scaled_height = image_height * scale_factor
+    story.append(Paragraph("Persönliche Informationen", style_heading))
+    story.append(personal_table)
+    story.append(Spacer(1, 0.2*inch))
 
-            table_data[6][1] = pdf.drawInlineImage(signature_image, 305, 730 - 50 * row_index - scaled_height, width=scaled_width, height=scaled_height)
+    # Sport selection table
+    story.append(Paragraph("Sportarten", style_heading))
+    sports = [
+        "Handball", "Mutter-/Kind", "Basketball", "Fußball", "Ballschule", "Boule",
+        "Turnen", "Seniorensport", "Tae Bo", "Judo", "Klettern", "Pilates", "Badminton",
+        "Volleyball", "Fitness", "Tischtennis", "Indica", "Power Workout", "Kinderturnen",
+        "Faustball", "Passives Mitglied"
+    ]
+    sport_table = Table([[f"[ ] {sport}"] for sport in sports], colWidths=300, rowHeights=20)
+    story.append(sport_table)
 
-    pdf.save()
+    # Address table
+    story.append(Paragraph("Adresse", style_heading))
+    address_info = [
+        ["Straße Hausnr.:", ""],
+        ["Stadt, Postleitzahl:", ""]
+    ]
+    address_table = Table(address_info, colWidths=[2*inch, 2*inch], rowHeights=0.25*inch)
+    story.append(address_table)
+
+    # Bank data table
+    story.append(Paragraph("Bankdaten", style_heading))
+    bank_info = [
+        ["IBAN:", ""],
+        ["BIC:", ""]
+    ]
+    bank_table = Table(bank_info, colWidths=[2*inch, 2*inch], rowHeights=0.25*inch)
+    story.append(bank_table)
+
+    # Konvertieren des PIL-Bildes in ein ReportLab-Bildobjekt
+    image_buffer = BytesIO()
+    signature_image.save(image_buffer, format="PNG")
+    img = PlatypusImage(image_buffer)
+    story.append(img)  # Das Bild wird der Geschichte hinzugefügt
+
+    doc.build(story)
 
 @app.route('/download_pdf', methods=['POST'])
 def download_pdf():
-    print("Route '/download_pdf' wurde aufgerufen.")
     signature_data = request.form['signature']
-
-
-
-
-
-
-
-
     file_path = os.path.join('static', 'fileAblage', 'unterschrift.pdf')
 
     generate_pdf(signature_data, file_path)
