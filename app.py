99ce4f9ac220a0f5031c864ca129786ac00287ff
Stephan <stephan.staege@yahoo.de>
2023-12-13 17:56:28 +0100

    -Datenhandling geswitcht von JS auf Handling via Python -app.py - route zum PDF deaktiviert -app.py - csv reader mit implementiert -pricelist.html - codeanpassung JS obsolete + flask data handling

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from pdf_generator import download_pdf
+import csv
 
 app = Flask(__name__)
 
@@ -9,14 +10,16 @@ app.static_folder = 'static'
 def index():
     return render_template('index.html')
 
-@app.route('/download_pdf', methods=['GET', 'POST'])
-def handle_pdf_download():
-    if request.method == 'POST':
-        # Hier rufen Sie Ihre modifizierte download_pdf-Funktion auf
-        return download_pdf()
-    else:
-        # Wenn es sich um eine GET-Anfrage handelt, leiten Sie z.B. zurück zur Hauptseite
-        return render_template('index.html')
+# Obsolete - wird nicht mehr benötigt, da die Funktion in price_list.html implementiert wurde
+#
+# @app.route('/download_pdf', methods=['GET', 'POST'])
+# def handle_pdf_download():
+#     if request.method == 'POST':
+#         # Hier rufen Sie Ihre modifizierte download_pdf-Funktion auf
+#         return download_pdf()
+#     else:
+#         # Wenn es sich um eine GET-Anfrage handelt, leiten Sie z.B. zurück zur Hauptseite
+#         return render_template('index.html')
     
 @app.route('/mitglied-werden')
 def mitglied_werden():
@@ -28,7 +31,12 @@ def mitgliedsantrag():
 
 @app.route('/pricelist')
 def pricelist():
-    return render_template('pricelist.html')
+    data = []
+    with open('static/fileAblage/data.csv', 'r', encoding='utf-8-sig') as file:
+        csv_reader = csv.reader(file, delimiter=';')
+        for row in csv_reader:
+            data.append(row)
+    return render_template('pricelist.html', data=data)
 
 
 if __name__ == '__main__':
