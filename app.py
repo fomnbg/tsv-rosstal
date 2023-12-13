961deb54eae6350fbed2dfefe1b3bfb637752163
Stephan <stephan.staege@yahoo.de>
2023-12-13 21:29:12 +0100

    - csv file gepusht - Bug fixing

--- app.py
+++ app.py
@@ -6,28 +6,31 @@ app = Flask(__name__)
 
 app.static_folder = 'static'
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Obsolete - wird nicht mehr benötigt, da die Funktion in price_list.html implementiert wurde
-#
-# @app.route('/download_pdf', methods=['GET', 'POST'])
-# def handle_pdf_download():
-#     if request.method == 'POST':
-#         # Hier rufen Sie Ihre modifizierte download_pdf-Funktion auf
-#         return download_pdf()
-#     else:
-#         # Wenn es sich um eine GET-Anfrage handelt, leiten Sie z.B. zurück zur Hauptseite
-#         return render_template('index.html')
-    
+
+@app.route('/download_pdf', methods=['GET', 'POST'])
+def handle_pdf_download():
+    if request.method == 'POST':
+        # Hier rufen Sie Ihre modifizierte download_pdf-Funktion auf
+        return download_pdf()
+    else:
+        # Wenn es sich um eine GET-Anfrage handelt, leiten Sie z.B. zurück zur Hauptseite
+        return render_template('index.html')
+
+
 @app.route('/mitglied-werden')
 def mitglied_werden():
     return render_template('mitglied-werden.html')
 
+
 @app.route('/mitgliedsantrag')
 def mitgliedsantrag():
     return render_template('mitgliedsantrag.html')
+
 
 @app.route('/pricelist')
 def pricelist():
