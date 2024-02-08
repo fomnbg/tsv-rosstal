8423784fce21bbee72ab5d413d8d56f6eae8384a
nicoRup <151018043+nicoRup@users.noreply.github.com>
2024-02-08 13:47:11 +0100

    Berechnung Fixt

--- static/js/mitgliedsantrag.js
+++ static/js/mitgliedsantrag.js
@@ -1,118 +1,96 @@
-var memberCost = 0;
-var totalCost = 0;
-var sportCostPassiv = 0;
-var sportMultiplier = 0;
-
-function kindOfSportChange(element) {
-  if (element.checked) {
-    sportMultiplier++;
-  } else {
-    sportMultiplier--;
-  }
-}
-
 window.onload = (event) => {
-  function checkAge() {
-    var parentSignature = document.getElementById('parentSignature');
-    var shouldShowParentSignature = false;
-
-    var birthdateFields = document.querySelectorAll('input[type="date"]');
-    for (var i = 0; i < birthdateFields.length; i++) {
-        var birthdate = new Date(birthdateFields[i].value);
-        var currentDate = new Date();
-        var age = currentDate.getFullYear() - birthdate.getFullYear();
-
-        if (age < 18) {
-            shouldShowParentSignature = true;
-            break;
-        }
-    }
-
-    if (shouldShowParentSignature) {
-        parentSignature.classList.remove('hidden-opacity');
-    } else {
-        parentSignature.classList.add('hidden-opacity');
-    }
-}
-
-document.addEventListener('change', function (event) {
-    if (event.target && event.target.matches('input[type="date"]')) {
-        checkAge();
-    }
-});
-
-
-  function berechneKosten() {
-    let totalCost = 0;
-    const isFamilyMembership = document.getElementById('family-membership').checked;
-    const processingFee = 15;
-
-    for (let i = 1; i <= 5; i++) {
-      let memberCost = calculateMemberCost(i, isFamilyMembership);
-      totalCost += memberCost * sportMultiplier;
-    }
-
-    const totalCostYearly = totalCost * 12;
-    const finalTotalCost = totalCostYearly + processingFee;
-
-    updateCostDisplay(totalCostYearly, processingFee, finalTotalCost);
-  }
-
-  function calculateMemberCost(memberIndex, isFamilyMembership) {
-    const birthdateElement = document.querySelector(`#personalDataField${memberIndex} input[type="date"]`);
-
-    if (!birthdateElement) {
-      return 0;
-    }
-
-    const birthdate = birthdateElement.value;
-    const age = calculateAge(birthdate);
-
-    if (document.getElementById('passivMember' + memberIndex) &&
-        document.getElementById('passivMember' + memberIndex).checked) {
-      return 7.5;
-    }
-
-    if (isFamilyMembership) {
-      return 19.17;
-    }
-
-    return calculateActiveMemberCost(age);
-  }
-
-
-  function calculateActiveMemberCost(age) {
-    if (age >= 4 && age <= 6) {
-      return 5.17;
-    } else if (age >= 7 && age <= 13) {
-      return 6.58;
-    } else if (age >= 14 && age <= 18) {
-      return 7.67;
-    } else if (age >= 18) {
-      return 10.83;
-    }
-    return 0;
-  }
-
-  function calculateAge(birthdate) {
-    const birthDate = new Date(birthdate);
-    const currentDate = new Date();
-    const age = currentDate.getFullYear() - birthDate.getFullYear();
-    return age;
-  }
-
-  function updateCostDisplay(totalCostYearly, processingFee, totalCost) {
-    document.getElementById('yearlyPrice').innerText = `${totalCostYearly.toFixed(2)}€`;
-    document.getElementById('processingFee').innerText = `${processingFee.toFixed(2)}€`;
-    document.getElementById('totalPrice').innerText = `${totalCost.toFixed(2)}€`;
-  }
-
-
-  var inputs = document.querySelectorAll('input');
-  inputs.forEach(function (input) {
-    input.addEventListener('input', function () {
-      setTimeout(berechneKosten, 500);
-    });
+  document.addEventListener('change', function (event) {
+      if (event.target && event.target.matches('input[type="date"]')) {
+          checkAge();
+      }
   });
 
-};
+  document.addEventListener('input', function() {
+    setTimeout(berechneKosten, 500);
+  });
+
+  document.querySelectorAll('#addFieldButton, #removeFieldButton').forEach(button => {
+    button.addEventListener('click', function() {
+      berechneKosten();
+    });
+  })
+}; 
+
+function checkAge() {
+  var parentSignature = document.getElementById('parentSignature');
+  var shouldShowParentSignature = false;
+  var birthdateFields = document.querySelectorAll('input[type="date"]');
+
+  for (var i = 0; i < birthdateFields.length; i++) {
+      var birthdate = new Date(birthdateFields[i].value);
+      var currentDate = new Date();
+      var age = currentDate.getFullYear() - birthdate.getFullYear();
+      if (age < 18) {
+          shouldShowParentSignature = true;
+          break;
+      }
+  }
+
+  if (shouldShowParentSignature) {
+      parentSignature.classList.remove('hidden-opacity');
+  } else {
+      parentSignature.classList.add('hidden-opacity');
+  }
+}
+
+function berechneKosten() {
+  var totalCost = 0;
+  var totalCostYearly = 0;
+  var processingFee = 0;
+  
+  if (document.querySelector('input[name="membership-type"]:checked').value === 'new-membership')
+  {
+    for (let i = 1; i <= 5; i++) {
+      const memberContainerId = `personalDataField${i}`;
+      const memberContainer = document.getElementById(memberContainerId);
+
+      if (memberContainer) {
+      
+        var selectedSports = document.querySelectorAll(`#Member${i} input[type="checkbox"]:checked`).length;
+        var isPassiveMember = document.getElementById('passivMember' + i).checked;
+
+        
+        if (selectedSports > 1 || (selectedSports > 0 && !isPassiveMember)) {
+          const birthdate = memberContainer.querySelector(`input[type="date"]`).value;
+          const InputData = new Date(birthdate);
+          const currentDate = new Date();
+          const differenceInMS = currentDate - InputData;
+
+        
+          var differenceYears = differenceInMS / (1000 * 60 * 60 * 24 * 365.25);
+          if (differenceYears >= 4 && differenceYears <= 6) {
+            totalCostYearly += 62;
+          } else if (differenceYears >= 7 && differenceYears <= 13) {
+            totalCostYearly += 79;
+          } else if (differenceYears >= 14 && differenceYears <= 18) {
+            totalCostYearly += 92;
+          } else if (differenceYears >= 18) {
+            totalCostYearly += 130;
+          }
+        }
+
+        if (document.getElementById('passivMember' + i).checked == true) {
+          totalCostYearly += 90;
+        }
+
+        processingFee += 15;
+      }
+    }      
+  } else if (document.querySelector('input[name="membership-type"]:checked').value === 'family-membership') {
+    
+    totalCostYearly = 230;
+    
+    processingFee = document.querySelectorAll('.PD-Container .flexPersonalDataField').length * 15;
+  }
+
+  totalCost = totalCostYearly + processingFee;
+
+  document.getElementById('yearlyPrice').innerText = totalCostYearly.toLocaleString('de-DE', { minimumFractionDigits: 2, maximumFractionDigits: 2 }) + '€';
+  document.getElementById('processingFee').innerText = processingFee.toLocaleString('de-DE', { minimumFractionDigits: 2, maximumFractionDigits: 2 }) + '€';
+  document.getElementById('totalPrice').innerText = totalCost.toLocaleString('de-DE', { minimumFractionDigits: 2, maximumFractionDigits: 2 }) + '€';
+}
